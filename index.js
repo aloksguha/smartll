53218a0a59b58cdba4ea492891aaceda70443ab4
Alok Guha <aguha@tibco.com>
2017-03-10 15:30:05 +0530

    Removing log statements.

--- index.js
+++ index.js
@@ -8,21 +8,3 @@ module.exports = {
   }
 }
 
-var lnl = module.exports.linkedList();
-
-lnl.push(0);
-lnl.push(1);
-lnl.push(2);
-lnl.push(3);
-lnl.push(4);
-lnl.printList();
-console.log('Size = '+lnl.getSize());
-console.log('First = '+lnl.getFirstNode().value);
-console.log('First = '+lnl.getLastNode().value);
-console.log();
-console.log(lnl.removeNodeAtIndex(4));
-console.log('Size = '+lnl.getSize());
-console.log('----')
-lnl.printList();
-
-
